9446c4fa7e331ff36e5e75109bf3267ff591f244
Asif Alli <alli.asif@gmail.com>
2024-03-19 08:00:10 -0400

    Added more tests

--- test/FibTests.kt
+++ test/FibTests.kt
@@ -19,6 +19,62 @@ class FibTests {
         assertEquals(0,fib.doFib(0))
     }
 
+    @Test
+    fun testFib1equals1() {
+
+        val fib = Fib()
+
+        assertEquals(1,fib.doFib(1))
+    }
+
+    @Test
+    fun testFib2equals1() {
+
+        val fib = Fib()
+
+        assertEquals(1,fib.doFib(2))
+    }
+
+    @Test
+    fun testFib3equals2() {
+
+        val fib = Fib()
+
+        assertEquals(2,fib.doFib(3))
+    }
+
+    @Test
+    fun testFib4equals3() {
+
+        val fib = Fib()
+
+        assertEquals(3,fib.doFib(4))
+    }
+
+    @Test
+    fun testFib5equals5() {
+
+        val fib = Fib()
+
+        assertEquals(5,fib.doFib(5))
+    }
+
+    @Test
+    fun testFib6equals8() {
+
+        val fib = Fib()
+
+        assertEquals(8,fib.doFib(6))
+    }
+
+    @Test
+    fun testFib7equals13() {
+
+        val fib = Fib()
+
+        assertEquals(13,fib.doFib(7))
+    }
+
 }
 
 
